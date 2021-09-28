804adcc278b03fd4efbae897c6437333c8a42a6f
Rohan Somadder <rohansomadder10@gmail.com>
2021-09-28 12:30:40 +0530

    Updated basic structure

--- Time-calculator/time_calculator.py
+++ Time-calculator/time_calculator.py
@@ -1,7 +1,17 @@
-def add_time(start, duration):
+def add_time(start, duration, day=None):
 
+    new_time = start
+    hours, mins = map(int, duration.split(':'))
 
-
-
-
+    if hours == '0' and mins == '00':
+        return new_time
+    elif int(hours) < 24:
+        pass
+    elif int(hours) == 24:
+        pass
+    else:
+        pass
     return new_time
+
+
+print(add_time("11:06 PM", "2:02"))
