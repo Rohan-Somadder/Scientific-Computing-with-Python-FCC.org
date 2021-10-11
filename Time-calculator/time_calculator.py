1afde765a896a0334ed729ba6aaa8ac6d26f94aa
Rohan Somadder <rohansomadder10@gmail.com>
2021-10-11 19:24:03 +0530

    Completed-Time Calculator

--- Time-calculator/time_calculator.py
+++ Time-calculator/time_calculator.py
@@ -4,92 +4,99 @@ Program to add given time with extra hours and minutes and return the result.
 
 days = ["Monday", "Tuesday", "Wednesday",
         "Thursday", "Friday", "Saturday", "Sunday"]
-time = ["AM", "PM"]
-
-
-def add_hours(current_hours, add_hours, ts=None, day=None):
-
-    # Todo: return [current_hours,ts] after calculations
-    if current_hours + add_hours < 12:
-        current_hours = current_hours + add_hours
-
-    elif 12 <= add_hours + current_hours < 24:
-        current_hours = add_hours + current_hours - 12
-        if ts:
-            ts = time[(time.index(ts) + 1) % 2]
-
-    elif add_hours + current_hours >= 24:
-        current_hours = add_hours + current_hours - 24
-        if day:
-            day = days[(days.index(day)+1) % 7]
-
-    return [current_hours, ts, day]
 
 
 def add_time(start, duration, day_name=None):
 
     new_time = start
-    new_hrs, new_mins = map(int, new_time.split()[0].split(':'))
+    st_hrs, st_mins = map(int, new_time.split()[0].split(':'))
     hours, mins = map(int, duration.split(':'))
     day_time = new_time.split()[1]  # AM or PM
+    extra = 0  # Extra hours for determinig the days
+
+    if day_time == "PM":
+        st_hrs += 12  # Converte to 24 hrs format
 
     if day_name:
         new_time += (", " + day_name)
-    # print(new_time,hours,mins)
+
     if day_name:
         day_name = day_name.lower().capitalize()
 
     if hours == 0 and mins == 0:
         return new_time
 
-    elif hours < 24:
-        if new_mins + mins >= 60:
-            new_hrs += (new_mins+mins)//60
-            mins = new_mins + mins - (((new_mins + mins)//60)*60)
-
+    if st_hrs + hours < 24:
+        if st_mins + mins >= 60:
+            st_hrs += (st_mins+mins)//60
+            st_mins = st_mins + mins - (((st_mins + mins)//60)*60)
         else:
-            mins = mins + new_mins
+            st_mins = st_mins + mins
+        st_hrs = st_hrs + hours
 
-        hours, day_time, day_name = map(
-            str, add_hours(hours, new_hrs, day_time, day_name))
-        day_name = None if day_name == 'None' else day_name
-        new_time = hours + ':' + (str(mins) if mins >= 10 else '0'+str(mins)
-                                  ) + ' ' + str(day_time) + ((', ' + day_name) if day_name else '')
-        # Todo : How to prevent (next day) from the text
-        #! Working incorrectly
-        print(new_time.split()[1])
-        if day_name != new_time.split()[1] and day_name is not None:
-            print(new_time, day_name)
-            new_time += ' (next day)'
+        if st_hrs <= 12:
+            new_time = str(st_hrs)
+        elif 12 < st_hrs < 24:
+            new_time = str(st_hrs - 12)
+        elif st_hrs >= 24:
+            new_time = str(st_hrs - 24 if st_hrs != 24 else st_hrs - 12)
+            extra += 1
 
-    elif hours == 24:
+        if st_mins < 10:
+            new_time += ':0'+str(st_mins)
+        else:
+            new_time += ':'+str(st_mins)
+
+        if 12 <= st_hrs < 24:
+            new_time += ' PM'
+        else:
+            new_time += ' AM'
+
         if day_name:
-            day_name = days[(days.index(day_name)+1) % 7]
-            new_time += ", " + day_name + " (next day)"
-
-        else:
-            new_time += ", (next day)"
+            if extra == 1:
+                new_time += ', ' + day_name + '(next day)'
+            else:
+                new_time += ', ' + day_name
 
     else:
-        extra = hours//24
-        hours = hours - (extra*24)
-        if new_mins + mins >= 60:
-            new_hrs += (new_mins+mins)//60
-            mins = new_mins + mins - (((new_mins + mins)//60)*60)
-
+        if st_mins + mins >= 60:
+            hours += (st_mins+mins)//60
+            st_mins = st_mins + mins - (((st_mins + mins)//60)*60)
         else:
-            mins = mins + new_mins
+            st_mins = st_mins + mins
 
-        hours, day_time, day_name = map(
-            str, add_hours(hours, new_hrs, day_time, day_name))
-        day_name = None if day_name == 'None' else day_name
-        new_time = hours + ':' + (str(mins) if mins >= 10 else '0' + str(mins)) + \
-            ' ' + str(day_time) + ', ' + (day_name if day_name else '')
-        if not day_name:
-            new_time += (' (next day)' if extra ==
-                         1 else f' ({extra} days later)')
+        extra = hours//24
+        hours = hours - extra*24
+        st_hrs = st_hrs + hours
+
+        if st_hrs < 12:
+            new_time = str(st_hrs)
+        elif 12 < st_hrs < 24:
+            new_time = str(st_hrs - 12)
+        elif st_hrs >= 24:
+            new_time = str(st_hrs - 24 if st_hrs != 24 else st_hrs - 12)
+            extra += 1
+
+        if st_mins < 10:
+            new_time += ':0'+str(st_mins)
+        else:
+            new_time += ':'+str(st_mins)
+
+        if 12 <= st_hrs < 24:
+            new_time += ' PM'
+        else:
+            new_time += ' AM'
+
+        if day_name:
+            new_time += ', ' + days[(days.index(day_name)+extra) % 7]
+            if extra == 1:
+                new_time += " (next day)"
+            elif extra > 1:
+                new_time += f" ({extra} days later)"
+        else:
+            if extra == 1:
+                new_time += " (next day)"
+            elif extra > 1:
+                new_time += f" ({extra} days later)"
 
     return new_time
-
-
-print(add_time("9:15 PM", "5:30"))  # == "2:45 AM (next day)")
