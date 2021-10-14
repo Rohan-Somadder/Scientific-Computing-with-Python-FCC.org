e98f9b01d27e76b4993f4f561f2728fc1a6d8c58
Rohan Somadder <rohansomadder10@gmail.com>
2021-10-14 20:16:48 +0530

    Budget App Completed

--- Budget-app/budget.py
+++ Budget-app/budget.py
@@ -10,22 +10,26 @@ class Category:
 
     def __str__(self):
         result = ''
-        result += '*'*((30-len(self.name))//2+(1 if len(self.name) % 2 == 0 else 0)) + \
-            self.name + '*'*((30-len(self.name))//2)
-        for a, m in zip(self.ledger):
+        result += self.name.center(30, '*') + '\n'
+        for item in self.ledger:
+            a, m = item['amount'], item['description']
             line = ''
             if len(m) <= 23:
                 line += m
                 if len(str(a)) <= 7:
-                    line += ' '*(30-len(m)+len(str(a))) + str(m)
+                    line += ' ' * \
+                        (30-(len(m)+len('{0:.2f}'.format(a)))
+                         ) + '{0:.2f}'.format(a)
                 else:
-                    line += ' '*(30-len(m)+7) + str(a)[:7]
+                    line += ' '*(30-(len(m)+7)) + '{0:.2f}'.format(a)[:7]
             else:
                 line += m[:23]
                 if len(str(a)) <= 7:
-                    line += ' '*(7-len(str(a))) + str(m)
+                    line += ' '*(7-len('{0:.2f}'.format(a))
+                                 ) + '{0:.2f}'.format(a)
                 else:
-                    line += str(a)[:7]
+                    line += '{0:.2f}'.format(a)[:7]
+            line.rstrip()
             line += '\n'
             result += line
         result += 'Total: '+str(self.total)
@@ -36,7 +40,7 @@ class Category:
         self.total += amount
 
     def withdraw(self, amount, message=''):
-        if self.check_funds(amount):
+        if not self.check_funds(amount):
             return False
         else:
             self.ledger.append({"amount": (-1*amount), "description": message})
@@ -49,7 +53,7 @@ class Category:
     def transfer(self, amount, category):
         if self.check_funds(amount):
             self.withdraw(amount, f'Transfer to {category.name}')
-            category.deposit(amount, f'Transfer from{self.name}')
+            category.deposit(amount, f'Transfer from {self.name}')
             return True
         else:
             return False
@@ -62,15 +66,44 @@ class Category:
 
 
 def create_spend_chart(categories):
-    w = 3*len(categories) + 1
+    w = 3*len(categories)
     result = ''
     result += 'Percentage spent by category\n'
     prices = {}
     total = 0.0
+
     for item in categories:
         price = 0.0
         for entry in item.ledger:
-            if entry['amount']<0:
+            if entry['amount'] < 0:
                 price -= entry['amount']
         prices[f'{item.name}'] = price
-        total += price 
+        total += price
+
+    for name, cost in prices.items():
+        prices[name] = int((cost/total*100)//10*10)
+
+    for i in range(0, 101, 10)[::-1]:
+        line = ''
+        line += str(i).rjust(3, ' ') + '| '
+        for name, perc in prices.items():
+            if perc >= i:
+                line += 'o  '
+            else:
+                line += '   '
+
+        line += '\n'
+        result += line
+    result += '    ' + '-'*(w+1) + '\n'
+
+    num = max([len(item.name) for item in categories])
+
+    for i in range(num):
+        line = '     '
+        for item in categories:
+            line += (item.name[i] if i < len(item.name) else ' ')
+            line += '  '
+        result += line
+        if i < num-1:
+            result += '\n'
+    return result
