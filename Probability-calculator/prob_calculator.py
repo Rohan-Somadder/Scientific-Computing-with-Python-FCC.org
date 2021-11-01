2a3f00c99c7d6e5c52d5900d00071497f01405bf
Rohan Somadder <rohansomadder10@gmail.com>
2021-11-01 21:29:42 +0530

    Prob Calc and Course Completed

--- Probability-calculator/prob_calculator.py
+++ Probability-calculator/prob_calculator.py
@@ -24,7 +24,7 @@ class Hat:
             '''
         else:
             lst = self.contents.copy()
-            #ṇself.contents = []
+            self.contents = []
         return lst
 
 
@@ -33,30 +33,11 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     correct = 0
     for name, num in expected_balls.items():
         exp_balls += [name]*num
-    exp_balls.sort()
     for _ in range(num_experiments):
         h = copy.deepcopy(hat)
         drawn_balls = h.draw(num_balls_drawn)
-        drawn_balls.sort()
-        # checking is correcting extra situations --- do a presciese match
-        balls_req = sum([1 for k, v in expected_balls.items() if drawn_balls.count(k) >= v])
+        balls_req = sum([1 for k, v in expected_balls.items()
+                        if drawn_balls.count(k) >= v])
         correct += 1 if balls_req == len(expected_balls) else 0
-        '''
-        if len(drawn_balls) == len(exp_balls):
-            result = (drawn_balls == exp_balls)
-        else:
-            result = all([(ele in exp_balls) for ele in drawn_balls]) if (len(drawn_balls) > len(
-                exp_balls)) else all([(ele in drawn_balls) for ele in exp_balls])
-        if result:
-            correct += 1
-        '''
+
     return correct/num_experiments
-
-
-# Testing
-# '''
-random.seed(95)
-h_ = Hat(blue=3, red=2, green=6)
-print(h_.contents)
-print(experiment(h_, {"blue": 2, "green": 1}, 4, 1000))
-# '''
