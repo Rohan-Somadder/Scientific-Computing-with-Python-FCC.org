3646fa4c4165a424b4542ffaee130d3f95f87594
Rohan Somadder <rohansomadder10@gmail.com>
2021-11-01 21:21:53 +0530

    updated - test 3 (3.78/2.73)

--- Probability-calculator/prob_calculator.py
+++ Probability-calculator/prob_calculator.py
@@ -10,11 +10,53 @@ class Hat:
         for colour, nos in kwargs.items():
             self.contents += [colour]*nos
 
+    def draw(self, nos):
+        lst = []
+        if nos < len(self.contents):
+            for _ in range(nos):
+                index = random.randrange(len(self.contents))
+                lst.append(self.contents[index])
+                self.contents.pop(index)
+            '''
+            lst = random.sample(self.contents, nos)
+            for item in lst:
+                self.contents.remove(item)
+            '''
+        else:
+            lst = self.contents.copy()
+            #ṇself.contents = []
+        return lst
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    exp_balls = []
+    correct = 0
+    for name, num in expected_balls.items():
+        exp_balls += [name]*num
+    exp_balls.sort()
+    for _ in range(num_experiments):
+        h = copy.deepcopy(hat)
+        drawn_balls = h.draw(num_balls_drawn)
+        drawn_balls.sort()
+        # checking is correcting extra situations --- do a presciese match
+        balls_req = sum([1 for k, v in expected_balls.items() if drawn_balls.count(k) >= v])
+        correct += 1 if balls_req == len(expected_balls) else 0
+        '''
+        if len(drawn_balls) == len(exp_balls):
+            result = (drawn_balls == exp_balls)
+        else:
+            result = all([(ele in exp_balls) for ele in drawn_balls]) if (len(drawn_balls) > len(
+                exp_balls)) else all([(ele in drawn_balls) for ele in exp_balls])
+        if result:
+            correct += 1
+        '''
+    return correct/num_experiments
 
 
 # Testing
-h = Hat(Red=1, blue=3)
-print(h.contents)
+# '''
+random.seed(95)
+h_ = Hat(blue=3, red=2, green=6)
+print(h_.contents)
+print(experiment(h_, {"blue": 2, "green": 1}, 4, 1000))
+# '''
